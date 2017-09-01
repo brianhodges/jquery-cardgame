3d0bdb4041ba7b7f39ea2983595c5c84476a824b
Brian Hodges <bhodges@PMIDSD.com>
2017-09-01 10:59:25 -0400

    more work (player hands)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,74 @@
 //Build a standard deck of cards
-deck = [];
-var suits = [
-	{ name: "Spades", unicode: "\u2664", color: "black" },
-	{ name: "Diamonds", unicode: "\u2662", color: "red" },
-	{ name: "Clubs", unicode: "\u2667", color: "black" },
-	{ name: "Hearts", unicode: "\u2661", color: "red" }
-];
-var ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
-
-for(x = 0; x < suits.length; x++) {
-	for(y = 0; y < ranks.length; y++) {
-		var card = {
-			color: suits[x].color,
-			unicode: suits[x].unicode,
-			suit: suits[x].name,
-			rank: ranks[y]
-		};
-		deck.push(card);
+function buildDeck() {
+	deck = [];
+	var suits = [
+		{ name: "Spades", unicode: "\u2664", color: "black" },
+		{ name: "Diamonds", unicode: "\u2662", color: "red" },
+		{ name: "Clubs", unicode: "\u2667", color: "black" },
+		{ name: "Hearts", unicode: "\u2661", color: "red" }
+	];
+	var ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+	
+	for(x = 0; x < suits.length; x++) {
+		for(y = 0; y < ranks.length; y++) {
+			var card = {
+				id: guid(),
+				color: suits[x].color,
+				unicode: suits[x].unicode,
+				suit: suits[x].name,
+				rank: ranks[y]
+			};
+			deck.push(card);
+		}
 	}
 }
 
-//Events for Displaying Cards
+//Events for Cards
 $(function() {
-	displayDeck();
-	$('#deck-size').text(deck.length);
+	dealDeck();
 });
 
-$(document).on('click', '#shuffle-button', function() {
+$(document).on('click', '#deal-button', function() {
+	dealDeck();
+});
+
+$(document).on('click', 'li', function() {
+	console.log(this.id);
+});
+
+//clears current game, shuffles, and deals
+function dealDeck() {
+	buildDeck();
 	shuffleDeck();
-});
-
-//clears current list, shuffles, and redisplays
-function shuffleDeck() {
-	$("ul").empty();
-	deck = shuffle(deck);
+	$('#player-1').empty();
+	$('#player-2').empty();
 	displayDeck();
+}
+
+function shuffleDeck() {
+	for (i = 0; i < 10; i++) {
+		deck = shuffle(deck);
+	}
 }
 
 //displays current deck to list
 function displayDeck() {
-	for(i = 0; i < deck.length; i++) {
-		var card_rank = /^[a-zA-Z()]+$/.test(deck[i].rank) ? deck[i].rank.substring(0,1) : deck[i].rank;
-		var el = "<li style='color:" + deck[i].color + "'>" + deck[i].unicode + card_rank + "</li>";
-		$('#deck-list').append(el);
+	for(x = 0; x < 5; x++) {
+		var card_rank = /^[a-zA-Z()]+$/.test(deck[x].rank) ? deck[x].rank.substring(0,1) : deck[x].rank;
+		var el = "<li id='" + deck[x].id + "' style='color:" + deck[x].color + "'>" +
+				"<span class='card-item'>" + deck[x].unicode + card_rank + "</li>";
+		$('#player-1').append(el);
+		deck.splice(x, 1);
 	}
+	shuffleDeck();
+	for(y = 0; y < 5; y++) {
+		var card_rank2 = /^[a-zA-Z()]+$/.test(deck[y].rank) ? deck[y].rank.substring(0,1) : deck[y].rank;
+		var el2 = "<li id='" + deck[y].id + "' style='color:" + deck[y].color + "'>" +
+				"<span class='card-item'>" + deck[y].unicode + card_rank2 + "</span></li>";
+		$('#player-2').append(el2);
+		deck.splice(y, 1);
+	}
+	$('#deck-size').text(deck.length);
 }
 
 //shuffles array
@@ -58,4 +83,15 @@ function shuffle(array) {
 		array[randomIndex] = temporaryValue;
 	}
 	return array;
+}
+
+//generates random GUID
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
